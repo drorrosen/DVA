bb675acc280df11808e11b5b88553e73b7281b85
Dror <dror.rosentraub@gmail.com>
2021-04-18 20:26:33 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request
+import pandas as pd
+import os
+
+port =  int(os.environ.get('PORT', 5000))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    message = request.form['message']
+    try:
+        zipcode = int(message)
+        print(zipcode)
+
+        df = pd.read_csv("zipcode_data_with_labels.csv")
+
+        my_prediction = df.loc[df['zipcodes'] == zipcode, 'avg_hier_labels']
+
+        if len(my_prediction) == 0:
+            my_prediction = "None"
+        else:
+            my_prediction = my_prediction.values[0]
+
+
+        return render_template('result.html', prediction=my_prediction)
+
+    except ValueError:
+        my_prediction = "Text"
+        return render_template('result.html', prediction=my_prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
